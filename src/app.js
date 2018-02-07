6b55049ddfbeb5678202d0f971fa1f7a9a19f194
erezstmn <erezstmn@gmail.com>
2018-02-07 11:52:32 +0200

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -10,24 +10,13 @@ import {Provider} from 'react-redux';
 import {setTextFilter} from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 
-
-
-let store  = configureStore();
-store.dispatch(addExpense({description: 'water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'rent', amount: 109500}));
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-
-console.log(visibleExpenses);
+const store  = configureStore();
 
 const jsx = (
     <Provider store={store}>
         <AppRouter />    
     </Provider>
-    
 );
-
 
 ReactDOM.render(jsx, document.getElementById('app'));
 
